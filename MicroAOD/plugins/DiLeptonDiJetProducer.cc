68b0d116e3a9dc7731f00f1cfed132a09b0818e6
GiuliaNegro <giulia.negro@cern.ch>
2016-08-31 19:51:23 +0200

    added request of at least 2 objects

--- MicroAOD/plugins/DiLeptonDiJetProducer.cc
+++ MicroAOD/plugins/DiLeptonDiJetProducer.cc
@@ -108,147 +108,166 @@ namespace flashgg {
 		//    cout << "evt.id().event()= " << evt.id().event() << "\tevt.isRealData()= " << evt.isRealData() << "\tmuonPointers.size()= " << muonPointers.size() << "\tpvPointers.size()= " << pvPointers.size() << endl;
 
 
-		for( unsigned int i = 0 ; i < electronPointers.size() ; i++ ) {
-			Electron_ptr electron1 = electronPointers[i];
-			double pt_ele1 = electron1->pt();
-			double eta_ele1 = electron1->eta();
-			if( pt_ele1 < minElePt_ || fabs( eta_ele1 ) > maxEleEta_ ) { continue; }
-			for( unsigned int j = i + 1 ; j < electronPointers.size() ; j++ ) {
-				Electron_ptr electron2 = electronPointers[j];
-				double pt_ele2 = electron2->pt();
-				double eta_ele2 = electron2->eta();
-				if( pt_ele2 < minElePt_ || fabs( eta_ele2 ) > maxEleEta_ ) { continue; }
 
-				for ( unsigned int l = 0 ; l < jetPointers.size() ; l++ ) {
-					Jet_ptr jet1 = jetPointers[l];
-					double pt_jet1 = jet1->pt();
-					double eta_jet1 = jet1->eta();
-					if( pt_jet1 < minJetPt_ || fabs( eta_jet1 ) > maxJetEta_ ) { continue; }
-					for( unsigned int h = l + 1 ; h < jetPointers.size() ; h++ ) {		
-						Jet_ptr jet2 = jetPointers[h];
-						double pt_jet2 = jet2->pt();
-						double eta_jet2 = jet2->eta();
-						if( pt_jet2 < minJetPt_ || fabs( eta_jet2 ) > maxJetEta_ ) { continue; }
+		if (electronPointers.size() > 1) {
 
-						DiLeptonDiJetCandidate DiEleDiJet( electron1, electron2, jet1, jet2, pvx);  //create DiLeptonDiJetCandidate with 2ele and 2jets
+			for( unsigned int i = 0 ; i < electronPointers.size() ; i++ ) {
+				Electron_ptr electron1 = electronPointers[i];
+				double pt_ele1 = electron1->pt();
+				double eta_ele1 = electron1->eta();
+				if( pt_ele1 < minElePt_ || fabs( eta_ele1 ) > maxEleEta_ ) { continue; }
+				for( unsigned int j = i + 1 ; j < electronPointers.size() ; j++ ) {
+					Electron_ptr electron2 = electronPointers[j];
+					double pt_ele2 = electron2->pt();
+					double eta_ele2 = electron2->eta();
+					if( pt_ele2 < minElePt_ || fabs( eta_ele2 ) > maxEleEta_ ) { continue; }
 
-						DiEleDiJet.setVtx( pvx );  
 
-						int ivtx = 0;
-						for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
-						 	if( pvx == primaryVertices->ptrAt( k ) ) {
-					    		ivtx = k;
-					    		break;
-					    	}
-					    }
-						DiEleDiJet.setVertexIndex( ivtx );
-						
-						DiLeptonDiJetColl->push_back( DiEleDiJet );  // store the DiEleDiJet into the collection
+					if (jetPointers.size() > 1) {
+						for ( unsigned int l = 0 ; l < jetPointers.size() ; l++ ) {
+							Jet_ptr jet1 = jetPointers[l];
+							double pt_jet1 = jet1->pt();
+							double eta_jet1 = jet1->eta();
+							if( pt_jet1 < minJetPt_ || fabs( eta_jet1 ) > maxJetEta_ ) { continue; }
+							for( unsigned int h = l + 1 ; h < jetPointers.size() ; h++ ) {		
+								Jet_ptr jet2 = jetPointers[h];
+								double pt_jet2 = jet2->pt();
+								double eta_jet2 = jet2->eta();
+								if( pt_jet2 < minJetPt_ || fabs( eta_jet2 ) > maxJetEta_ ) { continue; }
+
+								DiLeptonDiJetCandidate DiEleDiJet( electron1, electron2, jet1, jet2, pvx);  //create DiLeptonDiJetCandidate with 2ele and 2jets
+
+								DiEleDiJet.setVtx( pvx );  
+
+								int ivtx = 0;
+								for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
+								 	if( pvx == primaryVertices->ptrAt( k ) ) {
+							    		ivtx = k;
+							    		break;
+							    	}
+							    }
+								DiEleDiJet.setVertexIndex( ivtx );
+								
+								DiLeptonDiJetColl->push_back( DiEleDiJet );  // store the DiEleDiJet into the collection
+							}
+						}
 					}
-				}
 
-				for ( unsigned int l = 0 ; l < trackPointers.size() ; l++ ) {
-					Track_ptr track1 = trackPointers[l];
-					double pt_track1 = track1->pt();
-					double eta_track1 = track1->eta();
-					if( pt_track1 < minJetPt_ || fabs( eta_track1 ) > maxJetEta_ ) { continue; }
-					for( unsigned int h = l + 1 ; h < trackPointers.size() ; h++ ) {		
-						Track_ptr track2 = trackPointers[h];
-						double pt_track2 = track2->pt();
-						double eta_track2 = track2->eta();
-						if( pt_track2 < minJetPt_ || fabs( eta_track2 ) > maxJetEta_ ) { continue; }
 
-						DiLeptonDiJetCandidate DiEleDiTrack( electron1, electron2, track1, track2, pvx);  //create DiLeptonDiJetCandidate with 2ele and 2tracks
+					if (trackPointers.size() > 1) {
+						for ( unsigned int l = 0 ; l < trackPointers.size() ; l++ ) {
+							Track_ptr track1 = trackPointers[l];
+							double pt_track1 = track1->pt();
+							double eta_track1 = track1->eta();
+							if( pt_track1 < minJetPt_ || fabs( eta_track1 ) > maxJetEta_ ) { continue; }
+							for( unsigned int h = l + 1 ; h < trackPointers.size() ; h++ ) {		
+								Track_ptr track2 = trackPointers[h];
+								double pt_track2 = track2->pt();
+								double eta_track2 = track2->eta();
+								if( pt_track2 < minJetPt_ || fabs( eta_track2 ) > maxJetEta_ ) { continue; }
 
-						DiEleDiTrack.setVtx( pvx );
+								DiLeptonDiJetCandidate DiEleDiTrack( electron1, electron2, track1, track2, pvx);  //create DiLeptonDiJetCandidate with 2ele and 2tracks
 
-						int ivtx = 0;
-						for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
-						 	if( pvx == primaryVertices->ptrAt( k ) ) {
-					    		ivtx = k;
-					    		break;
-					    	}	
-					    }
-						DiEleDiTrack.setVertexIndex( ivtx );						
+								DiEleDiTrack.setVtx( pvx );
 
-						DiLeptonDiJetColl->push_back( DiEleDiTrack );  // store the DiEleDiTrack into the collection
+								int ivtx = 0;
+								for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
+								 	if( pvx == primaryVertices->ptrAt( k ) ) {
+							    		ivtx = k;
+							    		break;
+							    	}	
+							    }
+								DiEleDiTrack.setVertexIndex( ivtx );						
+
+								DiLeptonDiJetColl->push_back( DiEleDiTrack );  // store the DiEleDiTrack into the collection
+							}
+						}
 					}
+
 				}
 			}
-		}
+		} 
 
 
 
-		for( unsigned int i = 0 ; i < muonPointers.size() ; i++ ) {
-			Muon_ptr muon1 = muonPointers[i];
-			double pt_mu1 = muon1->pt();
-			double eta_mu1 = muon1->eta();
-			if( pt_mu1 < minMuPt_ || fabs( eta_mu1 ) > maxMuEta_ ) { continue; }
-			for( unsigned int j = i + 1 ; j < muonPointers.size() ; j++ ) {
-				Muon_ptr muon2 = muonPointers[j];
-				double pt_mu2 = muon2->pt();
-				double eta_mu2 = muon2->eta();
-				if( pt_mu2 < minMuPt_ || fabs( eta_mu2 ) > maxMuEta_ ) { continue; }
+		if (muonPointers.size() > 1) {
 
-				for ( unsigned int l = 0 ; l < jetPointers.size() ; l++ ) {
-					Jet_ptr jet1 = jetPointers[l];
-					double pt_jet1 = jet1->pt();
-					double eta_jet1 = jet1->eta();
-					if( pt_jet1 < minJetPt_ || fabs( eta_jet1 ) > maxJetEta_ ) { continue; }
-					for( unsigned int h = l + 1 ; h < jetPointers.size() ; h++ ) {		
-						Jet_ptr jet2 = jetPointers[h];
-						double pt_jet2 = jet2->pt();
-						double eta_jet2 = jet2->eta();
-						if( pt_jet2 < minJetPt_ || fabs( eta_jet2 ) > maxJetEta_ ) { continue; }
+			for( unsigned int i = 0 ; i < muonPointers.size() ; i++ ) {
+				Muon_ptr muon1 = muonPointers[i];
+				double pt_mu1 = muon1->pt();
+				double eta_mu1 = muon1->eta();
+				if( pt_mu1 < minMuPt_ || fabs( eta_mu1 ) > maxMuEta_ ) { continue; }
+				for( unsigned int j = i + 1 ; j < muonPointers.size() ; j++ ) {
+					Muon_ptr muon2 = muonPointers[j];
+					double pt_mu2 = muon2->pt();
+					double eta_mu2 = muon2->eta();
+					if( pt_mu2 < minMuPt_ || fabs( eta_mu2 ) > maxMuEta_ ) { continue; }
 
-						DiLeptonDiJetCandidate DiMuonDiJet( muon1, muon2, jet1, jet2, pvx);  //create DiLeptonDiJetCandidate with 2muons and 2jets
 
-						DiMuonDiJet.setVtx( pvx );
+					if (jetPointers.size() > 1) {
+						for ( unsigned int l = 0 ; l < jetPointers.size() ; l++ ) {
+							Jet_ptr jet1 = jetPointers[l];
+							double pt_jet1 = jet1->pt();
+							double eta_jet1 = jet1->eta();
+							if( pt_jet1 < minJetPt_ || fabs( eta_jet1 ) > maxJetEta_ ) { continue; }
+							for( unsigned int h = l + 1 ; h < jetPointers.size() ; h++ ) {		
+								Jet_ptr jet2 = jetPointers[h];
+								double pt_jet2 = jet2->pt();
+								double eta_jet2 = jet2->eta();
+								if( pt_jet2 < minJetPt_ || fabs( eta_jet2 ) > maxJetEta_ ) { continue; }
 
-						int ivtx = 0;
-						for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
-						 	if( pvx == primaryVertices->ptrAt( k ) ) {
-					    		ivtx = k;
-					    		break;
-					    	}	
+								DiLeptonDiJetCandidate DiMuonDiJet( muon1, muon2, jet1, jet2, pvx);  //create DiLeptonDiJetCandidate with 2muons and 2jets
+
+								DiMuonDiJet.setVtx( pvx );
+
+								int ivtx = 0;
+								for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
+								 	if( pvx == primaryVertices->ptrAt( k ) ) {
+							    		ivtx = k;
+							    		break;
+							    	}	
+								}
+								DiMuonDiJet.setVertexIndex( ivtx );
+
+								DiLeptonDiJetColl->push_back( DiMuonDiJet );  // store the DiMuonDiJet into the collection
+							}
 						}
-						DiMuonDiJet.setVertexIndex( ivtx );
-
-						DiLeptonDiJetColl->push_back( DiMuonDiJet );  // store the DiMuonDiJet into the collection
 					}
-				}
 
 
-				for ( unsigned int l = 0 ; l < trackPointers.size() ; l++ ) {
-					Track_ptr track1 = trackPointers[l];
-					double pt_track1 = track1->pt();
-					double eta_track1 = track1->eta();
-					if( pt_track1 < minJetPt_ || fabs( eta_track1 ) > maxJetEta_ ) { continue; }
-					for( unsigned int h = l + 1 ; h < trackPointers.size() ; h++ ) {		
-						Track_ptr track2 = trackPointers[h];
-						double pt_track2 = track2->pt();
-						double eta_track2 = track2->eta();
-						if( pt_track2 < minJetPt_ || fabs( eta_track2 ) > maxJetEta_ ) { continue; }
+					if (trackPointers.size() > 1) {
+						for ( unsigned int l = 0 ; l < trackPointers.size() ; l++ ) {
+							Track_ptr track1 = trackPointers[l];
+							double pt_track1 = track1->pt();
+							double eta_track1 = track1->eta();
+							if( pt_track1 < minJetPt_ || fabs( eta_track1 ) > maxJetEta_ ) { continue; }
+							for( unsigned int h = l + 1 ; h < trackPointers.size() ; h++ ) {		
+								Track_ptr track2 = trackPointers[h];
+								double pt_track2 = track2->pt();
+								double eta_track2 = track2->eta();
+								if( pt_track2 < minJetPt_ || fabs( eta_track2 ) > maxJetEta_ ) { continue; }
 
-						DiLeptonDiJetCandidate DiMuonDiTrack( muon1, muon2, track1, track2, pvx);  //create DiLeptonDiJetCandidate with 2muons and 2jets
+								DiLeptonDiJetCandidate DiMuonDiTrack( muon1, muon2, track1, track2, pvx);  //create DiLeptonDiJetCandidate with 2muons and 2jets
 
-						DiMuonDiTrack.setVtx( pvx );
+								DiMuonDiTrack.setVtx( pvx );
 
-						int ivtx = 0;
-						for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
-						 	if( pvx == primaryVertices->ptrAt( k ) ) {
-					    		ivtx = k;
-					    		break;
-					    	}	
+								int ivtx = 0;
+								for( unsigned int k = 0; k < primaryVertices->size() ; k++ ) {
+								 	if( pvx == primaryVertices->ptrAt( k ) ) {
+							    		ivtx = k;
+							    		break;
+							    	}	
+								}
+								DiMuonDiTrack.setVertexIndex( ivtx );
+
+								DiLeptonDiJetColl->push_back( DiMuonDiTrack );  // store the DiMuonDiTrack into the collection
+							}
 						}
-						DiMuonDiTrack.setVertexIndex( ivtx );
-
-						DiLeptonDiJetColl->push_back( DiMuonDiTrack );  // store the DiMuonDiTrack into the collection
 					}
+
 				}
-			}
-		}
-	
+			} 
+		} 
 
 		evt.put( DiLeptonDiJetColl );
 
