82bb75be72294068867d8ae4951a3c20a16212f1
GiuliaNegro <giulia.negro@cern.ch>
2016-11-21 14:52:59 +0100

    modified jobname instructions for WR signal MC

--- MetaData/work/prepareCrabJobs.py
+++ MetaData/work/prepareCrabJobs.py
@@ -305,7 +305,9 @@ if options.createCrabConfig:
             jobname = jobname.replace("reHLT_80X_mcRun2_asymptotic_v14-v1","reHLTasym16")
         if len(jobname) > 97:
             jobname = jobname.replace("_withHLT_80X_mcRun2_asymptotic_v14_ext1-v1","reHLTasym16")
-        ## per WR signal MC          
+        ## per WR signal MC      
+        if len(jobname) > 97:
+            jobname = jobname.replace("_GEN_SIM_13TeV","_13TeV")
         if len(jobname) > 97:
             jobname = jobname.replace("-b59cb78551aff289588aa5c69db4a3a1","")
         ## 
