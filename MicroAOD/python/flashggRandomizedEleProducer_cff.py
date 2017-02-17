2825fcb7f51c4ede73f13db7b05ce254359a2fd9
GiuliaNegro <giulia.negro@cern.ch>
2017-02-17 10:22:33 +0100

    created producer for randomized electrons to be launched on microAOD

---
+++ MicroAOD/python/flashggRandomizedEleProducer_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+# add the following lines to the module used to configure the RandomNumberGeneratorService
+# process.RandomNumberGeneratorService.flashggRandomizedEle = cms.PSet(
+#           initialSeed = cms.untracked.uint32(16253245)
+#         # engineName = cms.untracked.string('TRandom3') # optional, default to HepJamesRandom if absent
+#         )
+
+flashggRandomizedEle = cms.EDProducer("FlashggRandomizedEleProducer",
+                                    src = cms.InputTag("flashggElectrons"),  
+                                    # labels of various gaussian random numbers with mean=0, sigma=1
+                                    # to be associated with the photon object
+                                    labels = cms.vstring("smearE")
+                                    )
